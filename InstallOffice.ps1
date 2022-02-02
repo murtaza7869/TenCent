1d4d3af1de709b458ec5ee5a203b8c69476c636c
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-02-02 11:32:29 -0800

    Update InstallOffice.ps1

--- InstallOffice.ps1
+++ InstallOffice.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest 'https://dl.dropboxusercontent.com/s/jdagxkbgubqilq2/setup.exe' -OutFile 'C:\Windows\temp\setup.exe'
-Invoke-WebRequest 'https://dl.dropboxusercontent.com/s/1yy4rzlky2nqz5s/configuration-Office365-x64.xml' -OutFile 'C:\Windows\temp\configuration-Office365-x64.xml'
-$arg="/configure C:\Windows\temp\configuration-Office365-x64.xml"
+Invoke-WebRequest 'https://github.com/murtaza7869/TenCent/raw/main/TencentOfficeConfig.xml' -OutFile 'C:\Windows\temp\TencentOfficeConfig.xml'
+$arg="/configure C:\Windows\temp\TencentOfficeConfig.xml"
 Start-Process -FilePath "C:\Windows\temp\setup.exe" $arg -WorkingDirectory "C:\Windows\temp"
